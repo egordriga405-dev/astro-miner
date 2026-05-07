71c6dd958eae933ff11a588ee54a5723e70401ed
egordriga405-dev <egordriga405@gmail.com>
2026-05-07 12:04:08 +0300

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/astro-miner-idle/',
+  base: '/astro-miner/',
   plugins: [react()],
   build: {
     outDir: 'dist',
