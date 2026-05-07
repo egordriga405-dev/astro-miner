fb309edfb8155f6b556f4d07dc0052175b80b209
egordriga405-dev <egordriga405@gmail.com>
2026-05-07 11:08:47 +0300

    Remove import of global styles in main.tsx
    Remove global styles import from main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/globals.css';
 
 const root = document.getElementById('root')!;
 ReactDOM.createRoot(root).render(
